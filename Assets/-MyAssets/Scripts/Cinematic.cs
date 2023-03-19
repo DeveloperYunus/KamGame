204bc3023a26b1ee995991c7bd14ce31273d7b0e
DeveloperYunus <113380693+DeveloperYunus@users.noreply.github.com>
2023-03-20 00:30:43 +0300

    20.03.2023
    Sahneler tamam lore seslendirmeleri eklenecek

---
+++ Assets/-MyAssets/Scripts/Cinematic.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using DG.Tweening;
+using System.Collections;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Cinematic : MonoBehaviour
+{
+    public GameObject fadeImage;
+    public GameObject picture1;
+    public GameObject picture2;
+    public GameObject picture3;
+
+    [Space(10)]
+    public CanvasGroup UIpanel;
+    public TextMeshProUGUI devamTxt;
+
+    [Space(10)]
+    public AudioSource clipSource;
+    public AudioClip trLore, enLore;
+
+    private void Start()
+    {
+        UIpanel.DOFade(0, 0f);
+        UIpanel.DOFade(1, 1f).OnComplete(() => UIpanel.interactable = true);
+    }
+
+    public void LanguageBtn(int index)
+    {
+        PlayerPrefs.SetInt("language", index);
+
+        if (index == 0) devamTxt.text = "Devam...";
+        else            devamTxt.text = "Continue...";
+    }
+    public void Continue()
+    {
+        UIpanel.DOFade(0, 1f);
+
+        fadeImage.GetComponent<CanvasGroup>().DOFade(0, 3f).SetDelay(2f);
+        StartCoroutine(MovePictue());
+    }
+
+    IEnumerator MovePictue()
+    {
+        if (PlayerPrefs.GetInt("language") == 0)        //dile göre lore'u seç ve sonra anlatmaya baþla
+            clipSource.clip = trLore;
+        else
+            clipSource.clip = enLore;
+
+        clipSource.Play();
+
+        yield return new WaitForSeconds(15);
+        fadeImage.GetComponent<CanvasGroup>().DOFade(1, 3f);
+        picture2.GetComponent<CanvasGroup>().DOFade(1, 0).SetDelay(3f);
+        fadeImage.GetComponent<CanvasGroup>().DOFade(0, 3f).SetDelay(3.1f);
+
+        yield return new WaitForSeconds(15);
+        fadeImage.GetComponent<CanvasGroup>().DOFade(1, 3f);
+        picture3.GetComponent<CanvasGroup>().DOFade(1, 0).SetDelay(3f);
+        fadeImage.GetComponent<CanvasGroup>().DOFade(0, 3f).SetDelay(3.1f);
+
+        yield return new WaitForSeconds(15);
+        SceneManager.LoadScene("1");        //cinematic den sonra 1. sahneyi yükle
+    }
+}
---
+++ Assets/-MyAssets/Scripts/FirstScene.cs
@@ -0,0 +1,122 @@
+using DG.Tweening;
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FirstScene : MonoBehaviour
+{
+    public Light2D lightt;
+    public CanvasGroup sceneFade, levelBtnsHldr;
+    public Transform levelBtnsPnl;
+
+
+    float time, lightMax, lightMin, lightNrml;
+    bool levelPnlOpen;
+
+    [Space(10)]
+    public TextMeshProUGUI resetTxt;
+    public TextMeshProUGUI yesTxt, levelsTxt;
+
+
+    private void Start()
+    {
+        levelPnlOpen = false;
+        time = 0;
+
+        lightNrml = lightt.intensity;
+        lightMin = lightNrml - 0.22f;
+        lightMax = lightNrml + 0.22f;
+
+        yesTxt.transform.parent.GetComponent<RectTransform>().DOScale(0, 0f);
+
+        sceneFade.gameObject.SetActive(true);
+        sceneFade.GetComponent<RectTransform>().DOScale(1, 0);
+        sceneFade.DOFade(1, 0f);
+        sceneFade.DOFade(0, 2f).OnComplete(() => sceneFade.GetComponent<RectTransform>().DOScale(0, 0));
+
+        levelBtnsHldr.DOFade(0, 0);
+        levelBtnsHldr.GetComponent<RectTransform>().DOScale(0, 0);
+
+        int a = PlayerPrefs.GetInt("whichLevel", 1);
+
+        for (int i = 0; i < a; i++)
+        {
+            levelBtnsPnl.GetChild(i).GetComponent<Button>().interactable = true;
+        }
+
+        if (PlayerPrefs.GetInt("language", 0) == 0)     // 0 = türkçe
+        {
+            resetTxt.text = "   Oyunu Sýfýrla";
+            yesTxt.text = "Evet !";
+            levelsTxt.text = "Bölümler...";
+        }
+        else
+        {
+            resetTxt.text = "   Reset The Game";
+            yesTxt.text = "Yes !";
+            levelsTxt.text = "Levels...";
+        }
+    }
+    private void Update()
+    {
+        if (time < Time.time)
+        {
+            time += 0.01f;
+            FirstOOP.LightSparkling(lightt, lightMax, lightMin, lightNrml, 0.02f);
+        }
+
+        if (Input.anyKeyDown && !levelPnlOpen)   //herhangi bir tuþa basýnca burasý çalýþýr
+            FirstLevel();
+    }
+
+    public void GoLevels(string index)
+    {
+        sceneFade.GetComponent<RectTransform>().DOScale(1, 0);
+        sceneFade.DOFade(1, 2f).OnComplete(() =>                        //sahneyi kapatan siyah ekran açýldýktan sonra
+        {
+
+            SceneManager.LoadScene(index);
+        });
+    }
+
+    public void FirstLevel()
+    {
+        if (PlayerPrefs.GetInt("firsOpen") == 0)     //oyun ilk kez açýlýyorsa Cinematic sahnesine gitsin
+        {
+            sceneFade.DOKill();
+
+            sceneFade.GetComponent<RectTransform>().DOScale(1, 0);
+            sceneFade.DOFade(1, 2f).OnComplete(() =>                        //sahneyi kapatan siyah ekran açýldýktan sonra
+            {
+
+                PlayerPrefs.SetInt("firsOpen", 1);
+                PlayerPrefs.SetInt("language", 1);      //dili ingilizceye ayarla
+                SceneManager.LoadScene("Cinematic");
+                               
+            });
+        }
+        else
+        {
+            levelBtnsHldr.GetComponent<RectTransform>().DOScale(1, 0);
+            levelBtnsHldr.DOFade(1, 1f);
+            levelPnlOpen = true;
+        }     
+    }
+
+    public void ResetGame()
+    {
+        yesTxt.transform.parent.GetComponent<RectTransform>().DOKill();
+        yesTxt.transform.parent.GetComponent<RectTransform>().DOScale(1, 0.8f).SetEase(Ease.OutBack);
+        yesTxt.transform.parent.GetComponent<RectTransform>().DOScale(0, 0.3f).SetEase(Ease.InBack).SetDelay(2f);
+    }
+
+    public void AbsouleteResetGame()
+    {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene("FirstLevel");
+    }
+}
